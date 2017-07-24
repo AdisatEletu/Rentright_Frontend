80fb55ca0fafb1dec6d51d26f8333749782f4c8d
Tosin Felixson Yusuf <felixsonyusuftosin@gmail.com>
2017-07-24 12:35:57 +0100

    resolved merge

--- src/components/pages/layouts/BioForm.js
+++ src/components/pages/layouts/BioForm.js
@@ -92,8 +92,8 @@ import { loadAllTenants, loadSpecificTenant, patchSpecificTenant, deleteSpecific
   </div>
   </div>
   :
-
-        <div className = "t-md-10 t-fullheight" >
+     <div className = "t-md-10 t-flex t-justify-space-between m-bottomx ">
+        <div className = "t-md-6 t-fullheight p-widget deeper" >
          <div className= "m-profile-setup t-flex t-flex-column">
              <div className= "t-flex t-flex-column t-md-10 t-justify-left ">
             <div className= "t-gray-darken-3-f mid t-h1 t-flex t-flex-row t-justify-space-between t-align-top "><span className= "">Update Bio Information</span>
@@ -109,6 +109,7 @@ import { loadAllTenants, loadSpecificTenant, patchSpecificTenant, deleteSpecific
             <span className= "t-gray-darken-1-f thin t-h2 t-lh-h2  m-topp">Pelase provide accurate information</span>
             </div>
        <CompletenessBar completeness = {this.state.completed} label = "Bio Info completeness" />
+          <div className ="t-md-10 p-widget widget-padding">
       <div className="m-form-hold">
       <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "textbox" size = "t-md-4" ownstate = {this.state} name = "first_name"  initialvalue = {this.state.first_name} icons = "" label = "First Name"/>
       <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "textbox" size = "t-md-4" ownstate = {this.state} name = "last_name"  initialvalue = {this.state.last_name} icons = "" label = "Last Name"/>
@@ -117,6 +118,8 @@ import { loadAllTenants, loadSpecificTenant, patchSpecificTenant, deleteSpecific
       <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "textbox" size = "t-md-4" ownstate = {this.state} name = "state_of_origin"  initialvalue = {this.state.state_of_origin} icons = "" label = "State Of Origin"/>
       <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "textbox" size = "t-md-4" ownstate = {this.state} name = "educational_level"  initialvalue = {this.state.educational_level} icons = "" label = "Educational Level"/>
       </div>
+      </div>
+       <div className=" p-widget t-md-10 widget-padding ">
         <div className="m-form-hold">
       <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "textbox" size = "t-md-9" ownstate = {this.state} name = "preferences"  initialvalue = {this.state.preferences} icons = "" label = "preferences"/>
       </div>
@@ -125,13 +128,15 @@ import { loadAllTenants, loadSpecificTenant, patchSpecificTenant, deleteSpecific
       <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "textbox" size = "t-md-4" ownstate = {this.state} name = "phone_number"  initialvalue = {this.state.phone_number} icons = "" label = "Phone Number"/>
       </div>
      <div className="m-form-hold">
-      <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "textbox" size = "t-md-4" ownstate = {this.state} name = "first_name"  initialvalue = {this.state.email} icons = "" label = "e-mail"/>
+      <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "textbox" size = "t-md-4" ownstate = {this.state} name = "email"  initialvalue = {this.state.email} icons = "" label = "e-mail"/>
       <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "textbox" size = "t-md-4" ownstate = {this.state} name = "address"  initialvalue = { this.state.address ? this.state.address.address : ''} icons = "" label = "Address"/>
         </div>
+     
        <div className="m-form-hold">
       <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "date" size = "t-md-9" ownstate = {this.state} name = "date_of_birth"  initialvalue = {this.state.date_of_birth} icons = "" label = "Date Of Birth"/>
-
+   </div>
         </div>
+   <div className ="t-md-10 p-widget ">
 <div className = "major-padding col-md-10 space">
 <div className = "m-heading increase">Social Questions</div>
  <div className ="m-sub ">Please select any of the below options that applies to you</div> 
@@ -140,6 +145,7 @@ import { loadAllTenants, loadSpecificTenant, patchSpecificTenant, deleteSpecific
 <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "radio" size = "s4" ownstate = {this.state}  name = "live_with_children"  initialvalue = {this.state.live_with_children} icons = "verified_user" label = "Do you live with your kids? "/>
 <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "radio" size = "s4" ownstate = {this.state} name = " marital_status"  initialvalue = {this.state.marital_status} icons = "verified_user" label = "Are you married?"/>
 <FormElements  onUpdate = {this.onUpdate.bind(this)} type = "radio" size = "s4" ownstate = {this.state} name = "live_with_spouse"  initialvalue = {this.state.live_with_spouse} icons = "verified_user" label = "Do you live with your spouse?"/>
+</div>
 </div>
 </div>{/* form wrapper*/} 
  <div className="m-formhold t-flex t-justify-right t-md-10">
@@ -152,7 +158,35 @@ import { loadAllTenants, loadSpecificTenant, patchSpecificTenant, deleteSpecific
   </div>
    </div>
        
-        
+     </div>   
+<div className = "full-height t-md-35">
+     <div className ="t-md-10 p-widget t-flex t-flex-column ">
+    <div className = "m-heading m-green-f"> <i className = "material-icons small">group</i>&nbsp;&nbsp;Get Noticed</div>
+ <div className ="m-sub margin-htop m-green-f">Share your profile with landlords</div> 
+ 
+     </div>
+   <div className ="t-md-10 p-widget t-flex t-flex-column ">
+<div className = "m-heading increase">General Information</div>
+ <div className ="m-sub margin-htop ">Check this board to confirm your current information</div> 
+<div className = "itemlist"><div className = "key">First Name</div><div className = "item">{this.props.myProfile.tenants.next_of_kin}</div></div>
+<div className = "itemlist"><div className = "key">Last Name</div><div className = "item">{this.props.myProfile.tenants.next_of_kin_number}</div></div>
+<div className = "itemlist"><div className = "key">Educational Level</div><div className = "item">{this.props.myProfile.tenants.smoking_status ? "Yes im a Smoker": "No i dont smoke"}</div></div>
+<div className = "itemlist"><div className = "key">Your special Preference in apartment</div><div className = "item">{this.props.myProfile.tenants.pet_status ? "Yes I have pets": "No i dont have pets"}</div></div>
+<div className = "itemlist"><div className = "key">Short bio</div><div className = "item">{this.props.myProfile.tenants.immigration_status ? "Yes im an immigrant": "No i am a citezin"}</div></div>
+<div className = "itemlist"><div className = "key">Telephone number</div><div className = "item">{this.props.myProfile.tenants.convicted_status? "Yes i have being Convicted": "No my records are clean"}</div></div>
+<div className = "itemlist"><div className = "key">E-mail</div><div className = "item">{this.props.myProfile.tenants.enterpreneural_status ? "Yes i own my business": "I am employed"}</div></div>
+<div className = "itemlist"><div className = "key">Address</div><div className = "item">{this.props.myProfile.tenants.employment_status ? "Yes i am emploed" : "No i am unemployed"}</div></div>
+<div className = "itemlist"><div className = "key"> State Of Origin</div><div className = "item">{this.props.myProfile.tenants.student_status? "Yes i am a student" : "No  i am not a student"}</div></div>
+
+<div className = "itemlist"><div className = "key">Date Of Birth</div><div className = "item">{this.props.myProfile.tenants.immigration_status ? "Yes im an immigrant": "No i am a citezin"}</div></div>
+<div className = "itemlist"><div className = "key">Do You Have Children ?</div><div className = "item">{this.props.myProfile.tenants.convicted_status? "Yes i have being Convicted": "No my records are clean"}</div></div>
+<div className = "itemlist"><div className = "key">Do You Live with your Children ?</div><div className = "item">{this.props.myProfile.tenants.enterpreneural_status ? "Yes i own my business": "I am employed"}</div></div>
+<div className = "itemlist"><div className = "key">Are you married</div><div className = "item">{this.props.myProfile.tenants.employment_status ? "Yes i am emploed" : "No i am unemployed"}</div></div>
+<div className = "itemlist"><div className = "key"> Do You Live with your spouse? </div><div className = "item">{this.props.myProfile.tenants.student_status? "Yes i am a student" : "No  i am not a student"}</div></div>
+</div>
+
+
+   </div>
    </div>
         
         );
