13db5b64e44a15351c2e8079cdd71010cd27a12b
amadosi-algorism <amadosi.odaibo@algorismng.com>
2017-08-11 16:08:01 +0100

    nanya

--- src/components/pages/account/landlord/properties/propertiesEditor/lease/LeaseEditHome.js
+++ src/components/pages/account/landlord/properties/propertiesEditor/lease/LeaseEditHome.js
@@ -1,19 +1,31 @@
 import React, {Component} from 'react';
 import queryString from 'query-string';
+import TenantLease from "./TenantLease";
+import EditLease from "./EditLease";
 
 class LeaseEditHome extends Component {
-    componentDidMount() {
-        const isPreview = queryString.parse(this.props.location.search).preview ? queryString.parse(this.props.location.search).preview : false;
 
-        if(isPreview){
-            alert(isPreview);
+    constructor(props){
+        super(props);
+        this.state = {
+            isPreview: false,
+        }
+    }
+
+    componentDidMount() {
+        const is_preview = queryString.parse(this.props.location.search).preview ? queryString.parse(this.props.location.search).preview : false;
+
+        if(is_preview && is_preview !== 'false'){
+            this.setState({isPreview: true});
         }
 
     }
 
     render() {
         return (
-            <div></div>
+            <div>
+                {this.state.isPreview ? <TenantLease/> : <EditLease/>}
+            </div>
         );
     }
 
