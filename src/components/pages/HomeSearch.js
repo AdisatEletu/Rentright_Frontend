5536d6639308354198ecd719f4e177de008b7163
hadis <adisateletu@yahoo.com>
2017-10-06 14:51:32 +0100

    air

--- src/components/pages/HomeSearch.js
+++ src/components/pages/HomeSearch.js
@@ -3,79 +3,88 @@
  */
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
-import { Anchor } from 'antd';
+import   fetch from 'isomorphic-fetch';
+
 class HomeSearch extends Component{
     constructor(props){
         super(props);
-        this.State={};
-
     }
 
-    componentWillMount (){
-
-            this.searchFunc()
-            /*({
-                units: response.body.search,
-                total: response.body.totalResults
-            });*/
-    }
-
-    componentDidMount (){
-
-    }
-
-    /*componentWillUpdate (){ (nextProps, nextState)
-
-    } */
-
-    componentWillReceiveProps (){
-
-    }
-
-    searchFunc (query){
-        let url= "https://rentright.herokuapp.com/api/rentright/units/query/?";
-        fetch(url).then((data) => {
-            this.setState ({query:{loading:false, error:false,results:data}});
-        }).catch((error)=>{
-            console.log(error)
-            this.setState({query:{loading:false, error:true, results:undefined}});
-
-        })
 
 
-
-    }
-
-    updateSearch(){
-        this.refs.query.value
-    }
-
-    render(){
+      render(){
 
         return(
 
 
-            <div className = "global-search t-flex t-align-center">
-                <div className="t-flex t-md-10 t-justify-space-around">
-                    <input className = "t-md-3" type = "text" placeholder = "Search by place"/>
-                    <select className = "t-md-3" type = "text" placeholder = "Search by property type">
-                        <option selected  className = "place">Apartment type</option>
-                        <option >
-                            Condo
-                        </option>
-                        <option>
-                            Flat
-                        </option>
-                        <option>
-                            Self-contain
-                        </option>
-                        <option>
-                           Duplex
-                        </option>
-                    </select>
-                    <input className = "t-md-3" style = {styles.nobd} type = "text" placeholder = "Budget"/>   </div>
-                <button style = {styles.button}> GO </button>
+            <div className="t-md-10 t-justify-space-between t-flex ">
+                <div className="home-search2 t-flex t-md-4 t-flex-column home-pad">
+
+                    <div className="home-search-header t-md-10 museo">
+                        Find Properties
+                    </div>
+                    <div className="home-search-des t-md-10 proxima">
+                        Explore properties that suits your Personality on RentRight
+                    </div>
+                    <label className="home-search-label museo">Location </label>
+                    <div className="home-search-items2 t-flex t-md-10">
+                        <input type="text" className="t-md-10 home-search-key" />
+                    </div>
+                    <div className="holderr t-md-10 t-flex t-justify-space-between">
+                        <div className="half-holder t-flex t-flex-column t-md-48">
+                            <label className="home-search-label museo">Price</label>
+                            <div className="home-search-items2 t-flex  ">
+                                <input type="text" className="t-md-10 home-search-key "/>
+                            </div>
+                        </div>
+
+                        <div className="half-holder t-flex t-md-48 t-flex-column">
+                            <label className="home-search-label museo">Size </label>
+                            <div className="home-search-items2 t-flex  ">
+                                <input type="text" className="t-md-10 home-search-key"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="holderr t-md-10 t-flex t-justify-space-between">
+                        <div className="half-holder t-flex t-flex-column t-md-48">
+                            <label className="home-search-label museo">Property Type</label>
+                            <div className="home-search-items2 t-flex  ">
+                                <select className="t-md-10 home-search-key">
+                                    <option selected className="home-search-key">
+
+                                    </option>
+                                    <option value="condo" >
+                                        Condo
+                                    </option>
+                                    <option value="flat">
+                                        Flat
+                                    </option>
+                                    <option value='self-contain'>
+                                        Self-contain
+                                    </option>
+                                    <option value='dulpex'>
+                                        Duplex
+                                    </option>
+                            </select>
+
+                            </div>
+                        </div>
+
+                        <div className="half-holder t-flex t-md-48 t-flex-column">
+                            <label className="home-search-label museo">Bedroom</label>
+                            <div className="home-search-items2 t-flex  ">
+                                <input type="text" className="t-md-9 home-search-key" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="home-search-button t-md-10 t-flex t-justify-center museo">
+                        <span>Find</span>
+                    </div>
+                </div>
             </div>
+
 
         );
 
@@ -88,16 +97,16 @@ const styles = {
         webkitBorderBottomRightRadius: '0px',
     },
     button: {
-        borderRadius: '0px',
+        borderRadius: '2px',
         borderStyle:'none',
         borderTopLeftRadius: '0px',
         webkitBorderBottomLeftRadius: '0px',
         height: '40px',
         padding: '0px 10px',
         backgroundColor: '#f6505c',
-        fontSize: '20px',
+        fontSize: '15px',
         lineHeight: '40px',
-        color: '#fff',
+        color: 'rgba(255,255,255,0.5)',
         fontFamily: "Museo",
         fontWeight: 400,
 
